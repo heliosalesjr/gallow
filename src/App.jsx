9c9b3160dbf6dd752aeec0ad611147ab54e950cf
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-11-21 19:13:16 -0300

    here we go

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Here we go!</h1>
+      <p>This is where this game starts... great things are coming!</p>
+    </>
+  )
+}
+
+export default App
