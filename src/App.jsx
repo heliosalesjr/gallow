966b64325c94ce20cf60095bb19915d075798be6
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-11-22 19:31:17 -0300

    hardcoded the status section

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Header from "./components/Header"
+import Status from "./components/Status";
 import './index.css';
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Header />
+      <Status />
     </>
   )
 }
---
+++ src/components/Status.jsx
@@ -0,0 +1,12 @@
+
+
+function Status() {
+  return (
+    <div className="bg-green-600 rounded-xl text-center m-4 p-4">
+        <h2 className="font-bold py-2 text-lg">You win!</h2>
+        <h2 className="font-thin py-2">Well done!</h2>
+    </div>
+  )
+}
+
+export default Status
