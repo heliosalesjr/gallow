7841ad265dda174a51645466ed7405e321efd17e
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-11-24 20:23:43 -0300

    added functionality to the keyboard also preventing duplicates

--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,13 +1,27 @@
+import { useState } from "react";
 
 const AlphabetGrid = () => {
   // Array de letras de A a Z
   const letters = Array.from({ length: 26 }, (_, i) => String.fromCharCode(65 + i));
 
+  const [guessedLetters, setGuessedLetters] = useState([]);
+    
+  console.log(guessedLetters);
+
+    function addGuessedLetter(letter) {
+        setGuessedLetters((prev) => 
+            prev.includes(letter) ? prev :
+            [...prev, letter]);
+
+    }
+
   return (
+    
     <>
         <div className="max-w-lg mx-auto flex flex-wrap justify-center gap-4 py-8">
         {letters.map((letter) => (
             <button
+            onClick={() => addGuessedLetter(letter)}
             key={letter}
             className="w-14 h-14 bg-yellow-400 text-black font-bold text-lg rounded-md shadow-md transition-transform transform hover:scale-110 hover:bg-yellow-500 focus:outline-none"
             >
